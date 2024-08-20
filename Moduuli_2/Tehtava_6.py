4bae690ba2f5345ead387074ad1e599a71c9b9ac
petja_pajunen <petja.pajunen@elisanet.fi>
2024-08-20 15:28:08 +0300

    commit

--- Moduuli_2/Tehtava_6.py
+++ Moduuli_2/Tehtava_6.py
@@ -1,4 +1,7 @@
 import random
+n1 = random.randint(0,9)
+n2 = random.randint(0,9)
+n3 = random.randint(0,9)
+print(f"{n1}{n2}{n3}")
 
-print(random.randint(0,9))
-print()
+print(f"\n{random.randint(1,6)}{random.randint(1,6)}{random.randint(1,6)}{random.randint(1,6)}")
